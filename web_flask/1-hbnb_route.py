88283b720c2a1ed68eb32d0f1c5c0f9abd2c9186
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-05-20 04:28:45 +0000

    hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """Display Hello HBNB"""
-    return 'Hello HBND'
+    return 'Hello HBND!'
 
 
 @app.route('/hbng', strict_slashes=False)
